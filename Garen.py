eb623d7e9e66d3b452337b795a1c3ec06b2e5a6d
손지오 <geosohn@yonsei.ac.kr>
2026-01-13 17:13:08 +0900

    Create Garen.py
    add Garen

---
+++ Garen.py
@@ -0,0 +1,13 @@
+from classes.champion import Champion
+from classes.skill.garen_q import GarenQ
+
+class Garen(Champion):
+    def __init__(self):
+        super().__init__(
+            name="Garen",
+            stat=[69, 38, 0, 32, 340],
+            minions=("soldier", 0),
+            skill=[
+                GarenQ()
+            ]
+        )
