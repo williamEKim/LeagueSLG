9afe5f9498925c6b94dc2b2452ea53dd6d45c08e
williamEkim <dongyeon0308@gmail.com>
2026-01-12 20:29:21 -0800

    updated skill list in champ

--- classes/champion.py
+++ classes/champion.py
@@ -1,17 +1,25 @@
 from typing import List
 from utils.stats import Stats
+from classes.skill import Skill
 
 
 class Champion:
-    def __init__(self, name:str, stat:List[int], minions:tuple[str, int]):
+    def __init__(
+        self, 
+        name:str='', 
+        stat:List[int]=[0,0,0,0,0], 
+        minions:tuple[str, int]=('',0),
+        skill:List[Skill]=[]
+    ):
         self.name: str = name
         self.stat: List[int] = stat
         self.minionType, self.minionCount = minions
+        self.skills = skill
 
     # returns the name of champion
     def getName(self) -> str:
         return self.name
-        
+
     # returns the tuple of minion type(str) and minion count(int) of current champinon
     def getMinion(self) -> tuple:
         return (self.minionType, self.minionCount) 
